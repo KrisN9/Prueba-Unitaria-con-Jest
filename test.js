93d1a12a55c9f2117bd847a9b821c1c656d4e23c
KrisN9 <kristinank992@gmail.com>
2022-11-26 08:41:07 +0000

    modificar funcion de yen a pound

--- test.js
+++ test.js
@@ -7,15 +7,15 @@ test('adds 14 + 9 to equal 23', () => {
 
 test("One euro should be 1.206 dollars", () => {
     const { fromEuroToDollar } = require('./app.js')
-    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+    expect(fromEuroToDollar(3.5)).toBe(4.2);
 });
 
-test("One dollar should be 127.9 yens", function(){
+test("One euro should be 1.2 dollars, and 127.9 yens", () => {
     const { fromDollarToYen } = require('./app.js')
-    expect(fromDollarToYen(4.0)).toBe(511.6);
+    expect(fromDollarToYen(4.2/1.2)).toBe(447.65);
 });
 
-test("One yen should be 0.8 pounds", function(){
+test("One euro should be 1.2 dollars, 127.9 yens, and 0.8 pounds", () => {
     const { fromYenToPound } = require('./app.js')
     expect(fromYenToPound(511.4)).toBe(409.12);
 });
